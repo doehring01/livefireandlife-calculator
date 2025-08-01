233a54e32ddcbfaee67c5f749b9b64eca3f331c0
Ashley Doehring <ashleydoehring@Ashleys-MacBook-Air.local>
2025-08-01 15:21:12 -0600

    Added Eleventy config and build scripts

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,16 @@
 module.exports = function(eleventyConfig) {
+  // Copy assets folder to _site
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes"
+    }
+  };
+};
+module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/styles.css");
 
